ca9ff04f42c0f3cac6df07ba69eae562e15c6eff
ishaandwivedi1234 <50709505+ishaandwivedi1234@users.noreply.github.com>
2020-06-16 11:58:02 +0530

    Update face_detect.py

--- face_detect.py
+++ face_detect.py
@@ -1,45 +1,64 @@
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
-# from imutils.video import VideoStream
+from imutils.video import VideoStream
 from playsound import playsound
+from gtts import gTTS
 import numpy as np
 import argparse
-# import imutils
+import imutils
 import time
 import cv2
 import os
-import datetime
+import datetime as dt
+import random
+import pyttsx3
+import winsound
+import pygame
 
 
 proto_txt_path = os.path.sep.join([r"C:\Users\Ishaan\Desktop\Juet Hacks",
  "deploy.prototxt.txt"])
+starter = os.path.sep.join([r"C:\Users\Ishaan\Desktop\Juet Hacks",
+"starter.mp3"])
+proceed = os.path.sep.join([r"C:\Users\Ishaan\Desktop\Juet Hacks",
+"proceed.wav"])
+sorry = os.path.sep.join([r"C:\Users\Ishaan\Desktop\Juet Hacks",
+"sorry.wav"])
 
-welcome = os.path.sep.join([r"C:\Users\Ishaan\Desktop\Juet Hacks",
-"welcome.mp3"])
 
-        #'deploy.prototxt'
+
+pygame.mixer.init()
+pygame.mixer.set_num_channels(8)
+voice =pygame.mixer.Channel(2)
+pygame.mixer.music.load(sorry)
+sound =pygame.mixer.Sound(sorry)
+sound1=pygame.mixer.Sound(proceed)
+
+
 model_path = os.path.sep.join([r"C:\Users\Ishaan\Desktop\Juet Hacks",
 "res10_300x300_ssd_iter_140000.caffemodel"])
 model=os.path.sep.join([r"C:\Users\Ishaan\Desktop\Juet Hacks",
 "mask_detector.model"])
-        #'res10_300x300_ssd_iter_140000.caffemodel'
 face_detector = cv2.dnn.readNetFromCaffe(proto_txt_path, model_path)
 mask_detector = load_model(model)
 
+
 class VideoCamera(object):
     def __init__(self):
-
-
         self.cap = cv2.VideoCapture(0)
+        self.lastTime=dt.datetime.now()
+        self.currentTime=dt.datetime.now()
+
 
     def __del__(self):
         self.cap.release()
 
     def get_frame(self):
+        # playsound(starter)
         while True :
             ret, frame = self.cap.read()
-            # frame = imutils.resize(frame, width=1080)
+            frame = imutils.resize(frame, width=500)
             (h, w) = frame.shape[:2]
             blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104, 177, 123))
 
@@ -75,24 +94,28 @@ class VideoCamera(object):
                 (mask, withoutMask) = result
 
                 label = ""
+
                 if mask > withoutMask:
 
                     label = "Mask"
                     color = (0, 255, 0)
-                    #playsound(welcome)
+                    if voice.get_busy() == False:
+                            voice.play(sound1)
+
+
                 else:
                     label = "No Mask"
                     color = (0, 0, 255)
-                    #playsound(welcome)
+                    if voice.get_busy() == False:
+                                 voice.play(sound)
+                            
 
                 cv2.putText(frame, label, (startX, startY-10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
                 cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-
+                self.cap.release;
                 # cv2.imshow("Frame", frame)
                 # key = cv2.waitKey(1) & 0xFF
 
-            # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-            # so we must encode it into JPEG in order to correctly display the
-            # video stream.
+
 
             return frame
